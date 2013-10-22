e552dc127022472346480e11c1eb1727cffca386
anhnhatuit@gmail.com <anhnhatuit@gmail.com@c003af3a-d73a-d091-8046-2a82ecc9bd3a>
2013-10-22 11:14:23 +0000

    vừa code thêm lớp HOCSINH,LOP,MONHOC

--- QuanLiHocSinh/QLHS.DAO/clsHOCSINH_DAO.cs
+++ QuanLiHocSinh/QLHS.DAO/clsHOCSINH_DAO.cs
@@ -27,5 +27,54 @@ namespace QLHS.DAO
             con.Close();
             return table;
         }
+        public void themHocsinh(clsHOCSINH_DTO hocsinh)
+        {
+            connection.kiemTraKetNoi(con);
+            SqlCommand command = new SqlCommand("SP_ThemHocSinh", con);
+            command.CommandType = CommandType.StoredProcedure;
+            command.Parameters.Add("@MAKHOI", SqlDbType.Char).Value = hocsinh.Makhoi;
+            command.Parameters.Add("@MANH", SqlDbType.VarChar).Value = hocsinh.Manh;
+            command.Parameters.Add("@MALOP", SqlDbType.Char).Value = hocsinh.Malop;
+            command.Parameters.Add("@MAHS", SqlDbType.Char).Value = hocsinh.Mahs;
+            command.Parameters.Add("@TENHS", SqlDbType.NVarChar).Value = hocsinh.Tenhs;
+            command.Parameters.Add("@NGSINHHS", SqlDbType.SmallDateTime).Value = hocsinh.Ngaysinhhs;
+            command.Parameters.Add("@GIOITINHHS", SqlDbType.Char).Value = hocsinh.Gioitinh;
+            command.Parameters.Add("@DIACHIHS", SqlDbType.NVarChar).Value = hocsinh.Diachihs;
+            command.Parameters.Add("@DIENTHOAIHS", SqlDbType.VarChar).Value = hocsinh.Dienthoaihs;
+            command.Parameters.Add("@NGNHAPHOC", SqlDbType.SmallDateTime).Value = hocsinh.Ngnhaphoc;
+            command.Parameters.Add("@HINHANHHS", SqlDbType.VarChar).Value = hocsinh.Hinhanhhs;
+            command.Parameters.Add("@DANTOC", SqlDbType.NVarChar).Value = hocsinh.Dantoc;
+            command.ExecuteNonQuery();
+            con.Close();
+        }
+        public void suaHocsinh(clsHOCSINH_DTO hocsinh)
+        {
+            connection.kiemTraKetNoi(con);
+            SqlCommand command = new SqlCommand("SP_SuaHocSinh", con);
+            command.CommandType = CommandType.StoredProcedure;
+            command.Parameters.Add("@MAKHOI", SqlDbType.Char).Value = hocsinh.Makhoi;
+            command.Parameters.Add("@MANH", SqlDbType.VarChar).Value = hocsinh.Manh;
+            command.Parameters.Add("@MALOP", SqlDbType.Char).Value = hocsinh.Malop;
+            command.Parameters.Add("@MAHS", SqlDbType.Char).Value = hocsinh.Mahs;
+            command.Parameters.Add("@TENHS", SqlDbType.NVarChar).Value = hocsinh.Tenhs;
+            command.Parameters.Add("@NGSINHHS", SqlDbType.SmallDateTime).Value = hocsinh.Ngaysinhhs;
+            command.Parameters.Add("@GIOITINHHS", SqlDbType.Char).Value = hocsinh.Gioitinh;
+            command.Parameters.Add("@DIACHIHS", SqlDbType.NVarChar).Value = hocsinh.Diachihs;
+            command.Parameters.Add("@DIENTHOAIHS", SqlDbType.VarChar).Value = hocsinh.Dienthoaihs;
+            command.Parameters.Add("@NGNHAPHOC", SqlDbType.SmallDateTime).Value = hocsinh.Ngnhaphoc;
+            command.Parameters.Add("@HINHANHHS", SqlDbType.VarChar).Value = hocsinh.Hinhanhhs;
+            command.Parameters.Add("@DANTOC", SqlDbType.NVarChar).Value = hocsinh.Dantoc;
+            command.ExecuteNonQuery();
+            con.Close();
+        }
+        public void xoaHocsinh(clsHOCSINH_DTO hocsinh)
+        {
+            connection.kiemTraKetNoi(con);
+            SqlCommand command = new SqlCommand("SP_XoaHocSinh", con);
+            command.CommandType = CommandType.StoredProcedure;
+            command.Parameters.Add("@MAHS", SqlDbType.Char).Value = hocsinh.Mahs;
+            command.ExecuteNonQuery();
+            con.Close();
+        }
     }
 }
--- QuanLiHocSinh/QLHS.DAO/clsLOP_DAO.cs
+++ QuanLiHocSinh/QLHS.DAO/clsLOP_DAO.cs
@@ -2,10 +2,68 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-
+using System.Data;
+using System.Data.SqlClient;
+using QLHS.DTO;
 namespace QLHS.DAO
 {
-    class clsLOP_DAO
+    public class clsLOP_DAO
     {
+        private clsConnectDatabase connection;
+        private SqlConnection con;
+        public clsLOP_DAO()
+        {
+            connection = new clsConnectDatabase();
+            con = connection.KetNoi();
+        }
+        public DataTable danhsachLop()
+        {
+            connection.kiemTraKetNoi(con);
+            DataTable table = new DataTable();
+            SqlCommand command = new SqlCommand("SP_ThongTinLop", con);
+            command.CommandType = CommandType.StoredProcedure;
+            SqlDataAdapter adapter = new SqlDataAdapter(command);
+            adapter.Fill(table);
+            con.Close();
+            return table;
+        }
+        public void themLop(clsLOP_DTO lop)
+        {
+            connection.kiemTraKetNoi(con);
+            SqlCommand command = new SqlCommand("SP_ThemLop", con);
+            command.CommandType = CommandType.StoredProcedure;
+            command.Parameters.Add("@MAKHOI", SqlDbType.Char).Value = lop.Makhoi;
+            command.Parameters.Add("@MANH", SqlDbType.VarChar).Value = lop.Manh;
+            command.Parameters.Add("@MALOP", SqlDbType.Char).Value = lop.Malop;
+            command.Parameters.Add("@TENLOP", SqlDbType.NVarChar).Value = lop.Tenlop;
+            command.Parameters.Add("@SISO", SqlDbType.VarChar).Value = lop.Siso;
+            command.Parameters.Add("@MAGV", SqlDbType.Char).Value = lop.Magv;
+            command.ExecuteNonQuery();
+            con.Close();
+        }
+        public void suaLop(clsLOP_DTO lop)
+        {
+            connection.kiemTraKetNoi(con);
+            SqlCommand command = new SqlCommand("SP_SuaLop", con);
+            command.CommandType = CommandType.StoredProcedure;
+            command.Parameters.Add("@MAKHOI", SqlDbType.Char).Value = lop.Makhoi;
+            command.Parameters.Add("@MANH", SqlDbType.VarChar).Value = lop.Manh;
+            command.Parameters.Add("@MALOP", SqlDbType.Char).Value = lop.Malop;
+            command.Parameters.Add("@TENLOP", SqlDbType.NVarChar).Value = lop.Tenlop;
+            command.Parameters.Add("@SISO", SqlDbType.VarChar).Value = lop.Siso;
+            command.Parameters.Add("@MAGV", SqlDbType.Char).Value = lop.Magv;
+            command.ExecuteNonQuery();
+            con.Close();
+        }
+        public void xoaLop(clsLOP_DTO lop)
+        {
+            connection.kiemTraKetNoi(con);
+            SqlCommand command = new SqlCommand("SP_SuaLop", con);
+            command.CommandType = CommandType.StoredProcedure;
+            command.Parameters.Add("@MALOP", SqlDbType.Char).Value = lop.Malop;
+            command.Parameters.Add("@MANH", SqlDbType.VarChar).Value = lop.Manh;
+            command.ExecuteNonQuery();
+            con.Close();
+        }
     }
 }
