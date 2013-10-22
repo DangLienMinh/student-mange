8096aaa6c60a4fe95ee1fa378a3a5f19d82665dd
anhnhatuit@gmail.com <anhnhatuit@gmail.com@c003af3a-d73a-d091-8046-2a82ecc9bd3a>
2013-10-22 11:14:23 +0000

    vừa code thêm lớp HOCSINH,LOP,MONHOC

--- QuanLiHocSinh/QLHS.DAO/clsMONHOC_DAO.cs
+++ QuanLiHocSinh/QLHS.DAO/clsMONHOC_DAO.cs
@@ -2,10 +2,63 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-
+using System.Data;
+using System.Data.SqlClient;
+using QLHS.DTO;
 namespace QLHS.DAO
 {
-    class clsMONHOC_DAO
+    public class clsMONHOC_DAO
     {
+        private clsConnectDatabase connection;
+        private SqlConnection con;
+        public clsMONHOC_DAO()
+        {
+            connection = new clsConnectDatabase();
+            con = connection.KetNoi();
+        }
+        public DataTable danhsachMonhoc()
+        {
+            connection.kiemTraKetNoi(con);
+            DataTable table = new DataTable();
+            SqlCommand command = new SqlCommand("SP_ThongTinMonHoc", con);
+            command.CommandType = CommandType.StoredProcedure;
+            SqlDataAdapter adapter = new SqlDataAdapter(command);
+            adapter.Fill(table);
+            con.Close();
+            return table;
+        }
+        public void themMonhoc(clsMONHOC_DTO monhoc)
+        {
+            connection.kiemTraKetNoi(con);
+            SqlCommand command = new SqlCommand("SP_ThemMonHoc", con);
+            command.CommandType = CommandType.StoredProcedure;
+            command.Parameters.Add("@MAMH", SqlDbType.VarChar).Value = monhoc.Mamh;
+            command.Parameters.Add("@TENMH", SqlDbType.NVarChar).Value = monhoc.Tenmh;
+            command.Parameters.Add("@HESOMH", SqlDbType.Char).Value = monhoc.Hesomh;
+            command.Parameters.Add("@SOTIET", SqlDbType.Char).Value = monhoc.Sotiet;
+            command.ExecuteNonQuery();
+            con.Close();
+        }
+        public void suaMonhoc(clsMONHOC_DTO monhoc)
+        {
+            connection.kiemTraKetNoi(con);
+            SqlCommand command = new SqlCommand("SP_SuaMonHoc", con);
+            command.CommandType = CommandType.StoredProcedure;
+            command.Parameters.Add("@MAMH", SqlDbType.VarChar).Value = monhoc.Mamh;
+            command.Parameters.Add("@TENMH", SqlDbType.NVarChar).Value = monhoc.Tenmh;
+            command.Parameters.Add("@HESOMH", SqlDbType.Char).Value = monhoc.Hesomh;
+            command.Parameters.Add("@SOTIET", SqlDbType.Char).Value = monhoc.Sotiet;
+            command.ExecuteNonQuery();
+            con.Close();
+        }
+        public void xoaMonhoc(clsMONHOC_DTO monhoc)
+        {
+            connection.kiemTraKetNoi(con);
+            SqlCommand command = new SqlCommand("SP_XoaMonHoc", con);
+            command.CommandType = CommandType.StoredProcedure;
+            command.Parameters.Add("@MAMH", SqlDbType.VarChar).Value = monhoc.Mamh;
+            command.ExecuteNonQuery();
+            con.Close();
+        }
     }
 }
